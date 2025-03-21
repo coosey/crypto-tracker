df4116bc0729eae23632655fa8bf877b3563572a
Matthew Nguyen <mnguyen@777part.com>
2025-03-21 09:37:15 -0700

    update changes to formattedDataRow component & chart

---
+++ components/trending-list/trending-card-item/index.tsx
@@ -0,0 +1,31 @@
+import styles from './index.module.scss';
+import { CarrotPriceChange } from 'components/carrot-price-change';
+
+interface Props {
+  imgSrc: string;
+  imgAlt: string;
+  name: string;
+  marketPrice: string;
+  percentageChange: number;
+}
+
+export const TrendingCardItem = ({
+  imgSrc,
+  imgAlt,
+  name,
+  marketPrice,
+  percentageChange,
+}: Props) => {
+  return (
+    <div key={imgAlt} className={styles?.['card-item']}>
+      <div className={styles?.['card-item_side']}>
+        <img className={styles?.['card-item_image']} src={imgSrc} alt={imgAlt} />
+        <p>{name}</p>
+      </div>
+      <div className={styles?.['card-item_side']}>
+        <div>${marketPrice}</div>
+        <CarrotPriceChange value={percentageChange} />
+      </div>
+    </div>
+  );
+};
