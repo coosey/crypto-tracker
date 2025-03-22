e94ad0995af2b48ca01f5edc99ca943bfb2e8009
Matthew Nguyen <mnguyen@777part.com>
2025-03-22 16:41:24 -0700

    added click handler to redirect router to coin[id] on trending list action

--- components/coin-id-components/trending-coins/index.tsx
+++ components/coin-id-components/trending-coins/index.tsx
@@ -3,15 +3,24 @@ import { TrendingCard } from 'components/trending-list/card';
 import useTrendingList from 'libs/hooks/useTrendingList';
 import { TrendingCardItem } from 'components/trending-list/card-item';
 import { formatNumberWithSubscriptZeros } from 'libs/helpers/formatNumbersWithSubscript';
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
 
 export const TrendingCoins = () => {
   const { trendingList } = useTrendingList();
+  const router = useRouter();
+
+  const handleClickTrendinCoin = useCallback((coinId: string) => {
+    router.push(`/coin/${coinId}`);
+  }, []);
+
   return (
     <div className={styles?.['wrapper']}>
       {trendingList?.coins?.map?.((coin) => {
         const item = coin?.item;
+        const coinName = item?.id?.toLowerCase?.();
         return (
-          <TrendingCard key={coin.item.coin_id}>
+          <TrendingCard key={item?.coin_id} onClick={() => handleClickTrendinCoin(coinName)}>
             {{
               list: (
                 <TrendingCardItem
