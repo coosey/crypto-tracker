65abe82f5369b8cb96f53255207122558a62d768
Matthew Nguyen <mnguyen@777part.com>
2025-03-24 16:14:49 -0700

    added trending coins list on coin[id] page + styling

--- components/coin-id-components/trending-coins/index.tsx
+++ components/coin-id-components/trending-coins/index.tsx
@@ -3,39 +3,30 @@ import { TrendingCard } from 'components/trending-list/card';
 import useTrendingList from 'libs/hooks/useTrendingList';
 import { TrendingCardItem } from 'components/trending-list/card-item';
 import { formatNumberWithSubscriptZeros } from 'libs/helpers/formatNumbersWithSubscript';
-import { useRouter } from 'next/router';
-import { useCallback } from 'react';
 
 export const TrendingCoins = () => {
   const { trendingList } = useTrendingList();
-  const router = useRouter();
-
-  const handleClickTrendinCoin = useCallback((coinId: string) => {
-    router.push(`/coin/${coinId}`);
-  }, []);
-
   return (
     <div className={styles?.['wrapper']}>
       {trendingList?.coins?.map?.((coin) => {
         const item = coin?.item;
-        const coinName = item?.id?.toLowerCase?.();
         return (
-          <TrendingCard key={item?.coin_id} onClick={() => handleClickTrendinCoin(coinName)}>
+          <TrendingCard key={coin.item.coin_id}>
             {{
               list: (
                 <TrendingCardItem
                   className={styles?.['wrapper__item']}
                   imgSrc={item?.thumb}
-                  imgAlt={item?.name}
-                  name={item?.name}
-                  marketPrice={formatNumberWithSubscriptZeros(item?.data?.price?.toString?.())}
+                  imgAlt={item?.name} 
+                  name={item?.name} 
+                  marketPrice={formatNumberWithSubscriptZeros(item?.data?.price?.toString?.())} 
                   percentageChange={item?.data?.price_change_percentage_24h?.usd}
                 />
-              ),
+              )
             }}
           </TrendingCard>
-        );
+        )
       })}
     </div>
-  );
+  )
 };
