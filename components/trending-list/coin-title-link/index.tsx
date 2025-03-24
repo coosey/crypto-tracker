8a944c8d096212e4e1eec3ce399f3d98389adbf9
Matthew Nguyen <mnguyen@777part.com>
2025-03-24 15:44:36 -0700

    refactor trending card list to include trending categories

--- components/trending-list/coin-title-link/index.tsx
+++ components/trending-list/coin-title-link/index.tsx
@@ -2,10 +2,14 @@ import { Group } from '@mantine/core';
 import { IconChevronRight } from '@tabler/icons-react';
 import styles from './index.module.scss';
 
-export const TrendingCardTitleLink = () => {
+interface Props {
+  title: string;
+}
+
+export const TrendingCoinTitleLink = ({ title }: Props) => {
   return (
     <Group justify="space-between" mt="md" mb="xs">
-      <h3>Trending Coins</h3>
+      <h3>{title}</h3>
       <span>
         <a className={styles?.['cardAnchor']} href="/trending">
           View more
