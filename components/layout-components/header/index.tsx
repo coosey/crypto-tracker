09aa4d9181b5e383ef6e5020c2d42ad69ebbc166
Matthew Nguyen <mnguyen@777part.com>
2025-03-31 12:01:44 -0700

    title link to homepage

--- components/layout-components/header/index.tsx
+++ components/layout-components/header/index.tsx
@@ -1,6 +1,7 @@
 import styles from './index.module.scss';
 import cn from 'classnames';
 import { SignInComponent } from './sign-in';
+import Link from 'next/link';
 
 interface Props {
   className?: string;
@@ -9,7 +10,9 @@ interface Props {
 export const HeaderComponent = ({ className }: Props) => {
   return (
     <div className={cn(styles['header'], { className: className })}>
-      <div className={styles?.['logo']}>Crypto Tracker</div>
+      <Link href="/" className={styles?.['logo-wrapper']}>
+        <div className={styles?.['logo']}>Crypto Tracker</div>
+      </Link>
       <div className={styles?.['sign-in-wrapper']}>
         <SignInComponent />
       </div>
