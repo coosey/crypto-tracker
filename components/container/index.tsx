aadb246bb4ec1a89f638575bf8525dce030e555c
Matthew <itsmatthewnguyen@gmail.com>
2025-07-25 12:02:05 -0700

    add favicon images

--- components/container/index.tsx
+++ components/container/index.tsx
@@ -17,6 +17,10 @@ export default function Container({ children }: ContainerProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://api.coingecko.com" rel="dns-prefetch" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       {children}
     </>
